be616b5d9b7102a7506c9f9e81848ed72eaff243
Isaiah Meek <50851883+eyezahhhh@users.noreply.github.com>
2025-05-26 15:29:35 +1200

    Update ryd.background.js

--- Extensions/combined/ryd.background.js
+++ Extensions/combined/ryd.background.js
@@ -68,23 +68,6 @@ api.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
-api.runtime.onInstalled.addListener((details) => {
-  if (
-    // No need to show changelog if its was a browser update (and not extension update)
-    details.reason === "browser_update" ||
-    // Chromium (e.g., Google Chrome Cannary) uses this name instead of the one above for some reason
-    details.reason === "chrome_update" ||
-    // No need to show changelog if developer just reloaded the extension
-    details.reason === "update"
-  ) {
-    return;
-  } else if (details.reason == "install") {
-    api.tabs.create({
-      url: api.runtime.getURL("/changelog/3/changelog_3.0.html"),
-    });
-  }
-});
-
 // api.storage.sync.get(['lastShowChangelogVersion'], (details) => {
 //   if (extConfig.showUpdatePopup === true &&
 //     details.lastShowChangelogVersion !== chrome.runtime.getManifest().version
